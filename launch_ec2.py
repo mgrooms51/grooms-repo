350a9f6d42d719d608edbb7bdde9bd9203417f34
M. Grooms jr <melvingrooms@gmail.com>
2023-04-13 02:15:32 +0000

    launch_ec2

---
+++ launch_ec2.py
@@ -0,0 +1,11 @@
+# how to create/ launch ec2 instance
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+ec2.create_instances( ImageId='ami-06e46074ae430fba6',
+    InstanceType='t2.micro',
+    
+    MaxCount=1,
+    
+    MinCount=1,)
